b9c010b3fa88b1f37750308de7cd724181230c50
VoloshynVlad <vladyslav.voloshyn2@nure.ua>
2017-06-02 20:17:29 +0300

    third commit

---
+++ WordReader/MainController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordReader
+{
+    class MainController
+    {
+        public string SelectedDocument { get; set; }
+        public string PathDB { get; set; }
+        public string PathForComparedDB { get; set; }
+    }
+}
