478317502e09f526e888a253a6c16ab5a9141374
VoloshynVlad <vladyslav.voloshyn2@nure.ua>
2017-06-02 06:59:52 +0300

    first

---
+++ WordReader/MainForm.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WordReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Parse Doc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(96, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save to  DB";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Select DB";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(221, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Список преподавателей";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Select Doc";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Список групп";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(15, 190);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(221, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(12, 241);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(224, 21);
+            this.comboBox3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Список предметов";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(239, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1001, 214);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(236, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "DB name: ";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(236, 431);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(1004, 214);
+            this.dataGridView2.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(236, 415);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "DB name: ";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(236, 380);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Select DB";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(318, 88);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Make Query";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(318, 380);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Make Query";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(18, 402);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(96, 23);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "Compare tables";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1249, 663);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Работа с консультациями онлайн без регистрации и смс";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ WordReader/MainForm.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using System.Data.SQLite;
+using System.IO;
+using System.Drawing;
+
+namespace WordReader
+{
+    public partial class MainForm : Form
+    {
+        List<Consultation> consultations = new List<Consultation>();
+        List<string> lecturers = new List<string>();
+        List<string> groups = new List<string>();
+        List<string> subjects = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            List<Word.Range> TablesRanges = new List<Word.Range>();
+
+            try
+            {
+                Word.Application word = new Word.Application();
+                object missing = Type.Missing;
+                object filename = label2.Text;
+                Word.Document doc = word.Documents.Open(ref filename, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+
+                var wordApp = new Microsoft.Office.Interop.Word.Application();
+
+                for (int i = 1; i <= doc.Tables.Count; i++)
+                {
+                    Word.Range TRange = doc.Tables[i].Range;
+                    TablesRanges.Add(TRange);
+                }
+
+                int c = 0;
+                string Name = "", Subject = "", Group = "", Date = "", Time = "", Place = "", Addition = "";
+
+                Boolean bInTable;
+                int p = doc.Paragraphs.Count;
+                for (int par = 1; par <= doc.Paragraphs.Count; par++)
+                {
+                    bInTable = false;
+                    Word.Range r = doc.Paragraphs[par].Range;
+
+
+                    foreach (Word.Range range in TablesRanges)
+                    {
+                        if (r.Start >= range.Start && r.Start <= range.End)
+                        {
+                            c++;
+
+                            if (c == 2 && r.Text.ToString() != "\r\a")
+                            {
+                                Name = r.Text.ToString();
+                                if (!lecturers.Contains(Name))
+                                    lecturers.Add(Name);
+                            }
+                            //проверяем название предмета
+                            if (c == 3 && r.Text.ToString() != "\r\a")
+                            {
+                                Subject = r.Text.ToString();
+                                if (!subjects.Contains(Subject))
+                                    subjects.Add(Subject);
+                            }
+                            //проверяем название группы
+                            if (c == 4 && r.Text.ToString() != "\r\a")
+                            {
+                                Group = r.Text.ToString();
+                                if (!groups.Contains(Group))
+                                    groups.Add(Group);
+                            }
+                            //проверяем дату
+                            if (c == 5 && r.Text.ToString() != "\r\a")
+                            {
+                                Date = r.Text.ToString();
+                            }
+                            //проверяем пару
+                            if (c == 6 && r.Text.ToString() != "\r\a")
+                            {
+                                Time = r.Text.ToString();
+                            }
+                            //проверяем место проведения конс
+                            if (c == 7 && r.Text.ToString() != "\r\a")
+                            {
+                                Place = r.Text.ToString();
+                            }
+                            //примечание
+                            if (c == 8)
+                            {
+                                if (r.Text.ToString() == "\r\a")
+                                    Addition = "-";
+                                else
+                                    Addition = r.Text.ToString();
+                            }
+                            if (c == 9)
+                            {
+                                Consultation cons = new Consultation(Name, Subject, Group, Date, Time, Place, Addition);
+                                consultations.Add(cons);
+                                c = 0;
+                            }
+
+                            bInTable = true;
+                            break;
+                        }
+                    }
+
+                    //                if (!bInTable)
+                    //                  MessageBox.Show("!!!!!! Not In Table - Paragraph number " + par.ToString() + ":" + r.Text);
+                }
+                doc.Close(false);
+                word.Quit(false);
+                wordApp.Quit(false);
+
+                if (word != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(word);
+                if (doc != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(doc);
+                if (wordApp != null)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(wordApp);
+                
+                doc = null;
+                word = null;
+                wordApp = null;
+                GC.Collect();
+                MessageBox.Show("done");
+
+
+                comboBox1.Items.Clear();
+                comboBox2.Items.Clear();
+                comboBox3.Items.Clear();
+
+                for (int i = 1; i < lecturers.Count; i++)
+                    comboBox1.Items.Add(lecturers[i].Trim(new Char[] { '\r', '\a' }));
+
+                for (int i = 1; i < groups.Count; i++)
+                    comboBox2.Items.Add(groups[i].Trim(new Char[] { '\r', '\a' }));
+
+                for (int i = 1; i < subjects.Count; i++)
+                    comboBox3.Items.Add(subjects[i].Trim(new Char[] { '\r', '\a' }));
+
+                comboBox1.SelectedIndex = 0;
+                comboBox2.SelectedIndex = 0;
+                comboBox3.SelectedIndex = 0;
+            }
+            catch { Exception ex; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label5.Text = "";
+            string name = DateTime.Now.ToString();
+            string databaseName = Application.StartupPath + @"\" + name.Replace(':', '-') + ".db";
+            //  label5.Text = "DB name: " + databaseName;
+            label5.Text = databaseName;
+
+
+            SQLiteConnection.CreateFile(databaseName);
+            MessageBox.Show(File.Exists(databaseName) ? "База данных создана" : "Возникла ошибка при создании базы данных");
+
+            SQLiteConnection connection =
+                           new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+
+            SQLiteCommand command =
+                    new SQLiteCommand("CREATE TABLE consultations (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, subject TEXT, groop TEXT, date TEXT, time TEXT, place TEXT, addition TEXT);", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+
+
+            SQLiteCommand insert_command = new SQLiteCommand("INSERT INTO 'consultations' ('name', 'subject', 'groop', 'date', 'time', 'place', 'addition') VALUES (?, ?, ?, ?, ?, ?, ?);", connection);
+
+            for (int i = 1; i < consultations.Count; i++)
+            {
+
+                insert_command.Parameters.Add("@Name", DbType.String);
+                insert_command.Parameters.AddWithValue("@Name", consultations[i].Lecturer.Trim(new Char[] { '\r', '\a' }));
+
+                insert_command.Parameters.Add("@Subject", DbType.String);
+                insert_command.Parameters.AddWithValue("@Subject", consultations[i].Subject.Trim(new Char[] { '\r', '\a' }));
+
+                insert_command.Parameters.Add("@Groop", DbType.String);
+                insert_command.Parameters.AddWithValue("@Groop", consultations[i].Group.Trim(new Char[] { '\r', '\a' }));
+
+                insert_command.Parameters.Add("@Date", DbType.String);
+                insert_command.Parameters.AddWithValue("@Date", consultations[i].Date.Trim(new Char[] { '\r', '\a' }));
+
+                insert_command.Parameters.Add("@Time", DbType.String);
+                insert_command.Parameters.AddWithValue("@Time", consultations[i].Time.Trim(new Char[] { '\r', '\a' }));
+
+                insert_command.Parameters.Add("@Place", DbType.String);
+                insert_command.Parameters.AddWithValue("@Place", consultations[i].Place.Trim(new Char[] { '\r', '\a' }));
+
+                insert_command.Parameters.Add("@Addition", DbType.String);
+                insert_command.Parameters.AddWithValue("@Addition", consultations[i].Addition.Trim(new Char[] { '\r', '\a' }));
+
+                insert_command.ExecuteNonQuery();
+            }
+            connection.Close();
+            MessageBox.Show("Готово");
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+
+            string databaseName = ofd.FileName.ToString();
+
+            SQLiteConnection connection =
+                         new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+
+            SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM 'consultations'", connection);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            //WHERE time = '2'
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                this.dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+            }
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Файлы Word (*.doc; *.docx) | *.doc; *.docx";
+            ofd.ShowDialog();
+            label2.Text = ofd.FileName.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+
+            string databaseName = ofd.FileName.ToString();
+
+            SQLiteConnection connection =
+                         new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+
+            SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM 'consultations'", connection);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            //WHERE time = '2'
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                this.dataGridView2.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
+
